bbcd7b764b8537eb74e4fe38a1ee8d91fb0d395a
Ramlan Mohamed Rinos <e21336@eng.pdn.ac.lk>
2025-08-18 09:58:20 +0530

    User model created

--- src/main/java/com/example/springbootdemo/controllers/UserController.java
+++ src/main/java/com/example/springbootdemo/controllers/UserController.java
@@ -1,14 +1,28 @@
 package com.example.springbootdemo.controllers;
 
+import com.example.springbootdemo.models.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/api/users")
 public class UserController {
+//    @GetMapping
+//    public String getUsers() {
+//        return "Hello! user";
+//    }
+
     @GetMapping
-    public String getUsers() {
-        return "Hello! user";
+    public List<User> getUsers() {
+        return Arrays.asList(
+                new User(1, "Rinos", "rns@gmail.com"),
+                new User(2, "Rosy", "ros@gmail.com")
+        );
     }
+
+
 }
