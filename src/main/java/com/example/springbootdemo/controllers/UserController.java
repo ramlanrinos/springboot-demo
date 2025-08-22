615da1c5244b5389df30faea09b3ee9b49c032ad
Ramlan Mohamed Rinos <e21336@eng.pdn.ac.lk>
2025-08-22 10:04:30 +0530

    Get all users via database

--- src/main/java/com/example/springbootdemo/controllers/UserController.java
+++ src/main/java/com/example/springbootdemo/controllers/UserController.java
@@ -1,6 +1,9 @@
 package com.example.springbootdemo.controllers;
 
+import com.example.springbootdemo.entity.UserEntity;
 import com.example.springbootdemo.models.User;
+import com.example.springbootdemo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,19 +14,22 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/api/users")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
 //    @GetMapping
 //    public String getUsers() {
 //        return "Hello! user";
 //    }
 
     @GetMapping
-    public List<User> getUsers() {
-        return Arrays.asList(
-                new User(1, "Rinos", "rns@gmail.com"),
-                new User(2, "Rosy", "ros@gmail.com"),
-                new User(3, "Mohamed", "mhd@gmail.com")
-        );
+    public List<UserEntity> getUsers() {
+//        return Arrays.asList(
+//                new User(1, "Rinos", "rns@gmail.com"),
+//                new User(2, "Rosy", "ros@gmail.com"),
+//                new User(3, "Mohamed", "mhd@gmail.com")
+//        );
+        return userRepository.findAll();
     }
-
 
 }
--- src/main/java/com/example/springbootdemo/entity/UserEntity.java
+++ src/main/java/com/example/springbootdemo/entity/UserEntity.java
@@ -20,4 +20,28 @@ public class UserEntity {
         this.username = username;
         this.email = email;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
