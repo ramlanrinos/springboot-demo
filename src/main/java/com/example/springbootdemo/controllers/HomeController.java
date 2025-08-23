b5d96dc93ef66cdfc5f7440bd837d681190cef02
Ramlan Mohamed Rinos <e21336@eng.pdn.ac.lk>
2025-08-23 16:35:40 +0530

    securityFilterChain method created for customize the security

---
+++ src/main/java/com/example/springbootdemo/controllers/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.springbootdemo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping
+    public String getHomePage() {
+        return "Welcome to Spring Boot Demo";
+    }
+}
