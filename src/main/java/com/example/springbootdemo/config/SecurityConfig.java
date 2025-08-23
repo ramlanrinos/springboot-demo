2b3e3652709bb783b5852329d99363f77bb01ba3
Ramlan Mohamed Rinos <e21336@eng.pdn.ac.lk>
2025-08-23 17:08:43 +0530

    InMemoryUsers created

--- src/main/java/com/example/springbootdemo/config/SecurityConfig.java
+++ src/main/java/com/example/springbootdemo/config/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,5 +23,25 @@ public class SecurityConfig {
                         .requestMatchers("/").permitAll()
         ).formLogin(form -> form.permitAll());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("ayifa")
+                .password(passwordEncoder.encode("user123"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("mohamed")
+                .password(passwordEncoder.encode("admin456"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
