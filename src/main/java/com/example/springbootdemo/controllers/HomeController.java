4cd8f1118c525ff9c8a4c0359c4c7fcafa1d20db
Ramlan Mohamed Rinos <e21336@eng.pdn.ac.lk>
2025-08-24 10:09:39 +0530

    a GetMapping created for dashboard page

--- src/main/java/com/example/springbootdemo/controllers/HomeController.java
+++ src/main/java/com/example/springbootdemo/controllers/HomeController.java
@@ -9,4 +9,9 @@ public class HomeController {
     public String getHomePage() {
         return "Welcome to Spring Boot Demo";
     }
+
+    @GetMapping("/dashboard")
+    public String getDashboardPage() {
+        return "Login successful";
+    }
 }
