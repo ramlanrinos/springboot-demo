8f4e3d26b3aee01bfb2feae1d01b7a6770a44275
Ramlan Mohamed Rinos <e21336@eng.pdn.ac.lk>
2025-08-28 16:36:49 +0530

    CustomUserDetailsService class created

--- src/main/java/com/example/springbootdemo/repositories/UserRepository.java
+++ src/main/java/com/example/springbootdemo/repositories/UserRepository.java
@@ -3,6 +3,9 @@ package com.example.springbootdemo.repositories;
 import com.example.springbootdemo.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    // JpaRepository query by the given column name (userName)
+    Optional<UserEntity> findByUserName(String username);
 }
---
+++ src/main/java/com/example/springbootdemo/services/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.springbootdemo.services;
+
+import com.example.springbootdemo.entity.UserEntity;
+import com.example.springbootdemo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Fetch the user from database
+        UserEntity user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new User(user.getUserName(), user.getPassword(), null);
+    }
+}
